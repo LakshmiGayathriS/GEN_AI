4f59aaf341151854424060699c69ac3bdefad893
LakshmiGayathriS <LakshmiGayathri.S@bigthinkcode.com>
2024-04-17 16:04:47 +0530

    metadata

--- milvus.py
+++ milvus.py
@@ -1,3 +1,4 @@
+import json
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_community.vectorstores import Milvus
 from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
@@ -13,7 +14,7 @@ documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=50)
 docs = text_splitter.split_documents(documents)
 
-print(docs[0])
+# print(docs[0])
 texts = docs[0].page_content.strip()
 embedder = SpacyEmbeddings(model_name="en_core_web_sm")
 # embeddings = embedder.embed_documents(texts)
@@ -21,6 +22,7 @@ embedder = SpacyEmbeddings(model_name="en_core_web_sm")
 #     print(f"Embedding for document {i+1}: {embedding}")
 
 for doc in docs:
+    doc.metadata["source"]=os.path.basename(doc.metadata['source'])
     vector_db = Milvus.from_documents(
         docs,
         embedder,
@@ -28,9 +30,19 @@ for doc in docs:
         connection_args={"host": os.getenv("MILVUS_HOST"), "port": os.getenv("MILVUS_PORT")},
     )
 
-query = "Jack"
-docs = vector_db.similarity_search_with_score(query)
-print(docs)
+query = "Where did Jack go"
+docs = vector_db.similarity_search_with_score(query, k=3)
+
+response = []
+for doc, score in docs:
+    response.append({
+        "page_content": doc.page_content.strip(),
+        "score": score,
+        "metadata": doc.metadata['source']
+    })
+
+print(json.dumps(response, indent=4))
+
 # print(docs[0].page_content)
 
 
